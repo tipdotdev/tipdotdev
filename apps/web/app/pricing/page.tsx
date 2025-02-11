263e081c51a37f2cc8c56971a7d08f3a2798f798
Kyle Dickey <k@kyle.so>
2025-02-11 13:40:19 -0700

    fix bg colors

--- apps/web/app/pricing/page.tsx
+++ apps/web/app/pricing/page.tsx
@@ -2,7 +2,7 @@ import TUINavbar from "@/components/tui/tui-navbar";
 
 export default function Page() {
     return (
-        <div className="flex min-h-screen flex-col bg-[#111] font-mono font-normal text-[#d7d7d7]">
+        <div className="flex min-h-screen flex-col font-mono font-normal text-[#d7d7d7]">
             <TUINavbar active="pricing" />
         </div>
     );
