2d4e415c92788cc2cc5aea2edab5ff45c37f19f6
Kyle Dickey <k@kyle.so>
2025-02-15 15:12:18 -0700

    restructure sign out page

--- apps/web/app/auth/sign-out/page.tsx
+++ apps/web/app/auth/sign-out/page.tsx
@@ -2,7 +2,6 @@ import { createServerClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-    const sb = await createServerClient();
-    sb.auth.signOut();
+    (await createServerClient()).auth.signOut();
     return redirect("/auth/sign-in");
 }
