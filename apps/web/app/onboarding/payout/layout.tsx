5c3e1d1a02fb8f80ace23e9265a6a34bbdfa5cf8
Kyle Dickey <k@kyle.so>
2025-05-29 15:57:59 -0600

    fix missing type

--- apps/web/app/onboarding/payout/layout.tsx
+++ apps/web/app/onboarding/payout/layout.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const profile = await getSelfProfile();
-    if (profile?.stripe_account_id) {
+    if (profile?.stripeAcctID) {
         redirect("/dashboard");
     }
 
