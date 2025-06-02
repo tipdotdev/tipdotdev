d03db1d84434ae3e15fb9e98880610bc0d4a7c14
Kyle Dickey <k@kyle.so>
2025-06-02 02:15:32 -0600

    fly stuff and workflows

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 
-const DATABASE_URL = process.env.DATABSE_URL!;
+const DATABASE_URL = process.env.DATABASE_URL!;
 
 export default defineConfig({
     out: "./drizzle",
