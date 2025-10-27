9941aee2707b270960e61f71a1c62bc9053a111e
Kyle Dickey <k@kyle.so>
2025-10-27 10:27:03 -0600

    update posthog to use cookieless

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -2,5 +2,6 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    defaults: "2025-05-24"
+    defaults: "2025-05-24",
+    cookieless_mode: "always"
 });
